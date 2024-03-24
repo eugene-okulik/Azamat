80e19b24f31553b073c7959fcd8e69503a30a4e5
Azamat <azmmatthew@gmail.com>
2024-03-24 14:40:57 +0300

    Homework 5 added folder and three tasks py files

---
+++ homework/azamat_far/lesson_5_azamat/task_1.py
@@ -0,0 +1,9 @@
+person = ["John", "Doe", "New York", "+1372829383739", "US"]
+name, last_name, city, phone, country = (
+    person[0],
+    person[1],
+    person[2],
+    person[3],
+    person[4],
+)
+print(name, last_name, city, phone, country, sep=", ")
