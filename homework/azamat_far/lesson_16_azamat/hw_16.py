45aa1bbb3c1beb0dc9d83657b0fa4822f55329c9
Azamat <azmmatthew@gmail.com>
2024-10-29 22:13:35 +0300

    fixed path to file, and db check

--- homework/azamat_far/lesson_16_azamat/hw_16.py
+++ homework/azamat_far/lesson_16_azamat/hw_16.py
@@ -15,25 +15,17 @@ db = mysql.connect(
 )
 cursor = db.cursor()
 
-# SQL запрос в БД
-cursor.execute("""SELECT s.name, s.`second_name`, g.title, b.title, s2.title,
-l.title, m.value FROM
-`groups` g JOIN students s  ON s.group_id = g.id
-JOIN books b ON s.id = b.taken_by_student_id
-JOIN marks m ON s.id = m.student_id
-JOIN lessons l ON m.lesson_id = l.id
-JOIN subjets s2 ON l.subject_id = s2.id
-WHERE s.name = 'Ivan' or s.name = 'Petr' or s.name = 'Mark'""")
-names = cursor.fetchall()
+# Переменная для пути к файлу
+base_path = os.path.dirname(__file__)
+hw_path = os.path.dirname(os.path.dirname(base_path))
+file_path = os.path.join(hw_path, 'eugene_okulik', 'Lesson_16', 'data.csv')
 
-# Считывание строк таблицы
-with open('F:\\py files\\study_AQA\\Azamat\\homework\\eugene_okulik\\Lesson_16\
-\\data.csv', newline='') as csv_file:
+with open(file_path, newline='') as csv_file:
     file_data = csv.DictReader(csv_file)
     data = []
     for row in file_data:
         data.append(row)
-# Создание кортежа со строкой из таблицы
+# Создание ключей и кортежа для проверки
 for i in range(4):
     name = data[i]['name']
     sec_name = data[i]['second_name']
@@ -42,17 +34,22 @@ for i in range(4):
     sub_title = data[i]['subject_title']
     les_title = data[i]['lesson_title']
     mr_value = data[i]['mark_value']
+    # print(data)
     tup = (
         name, sec_name, gr_title, b_title, sub_title, les_title, mr_value
     )
-    # Проверка наличия в 'names' строки из файла
-    for j in names:
-        if tup in names:
-            no_in_bd = i
-# вывод строк, которых нет в БД
-for k in range(4):
-    if k != no_in_bd:
-        print(data[k]['name'], data[k]['second_name'], end=' ')
-        print(data[k]['group_title'], data[k]['book_title'], end=' ')
-        print(data[k]['subject_title'], data[k]['lesson_title'], end=' ')
-        print(data[k]['mark_value'])
+    # Запрос в БД и проверка последующая наличия записи
+    cursor.execute(f"""SELECT s.name, s.`second_name`, g.title,
+    b.title, s2.title, l.title, m.value FROM
+    `groups` g JOIN students s  ON s.group_id = g.id
+    JOIN books b ON s.id = b.taken_by_student_id
+    JOIN marks m ON s.id = m.student_id
+    JOIN lessons l ON m.lesson_id = l.id
+    JOIN subjets s2 ON l.subject_id = s2.id
+    WHERE s.name = '{name}' AND s.`second_name` = '{sec_name}'
+    AND g.title = '{gr_title}' AND b.title = '{b_title}'
+    AND s2.title = '{sub_title}' AND l.title = '{les_title}'
+    AND m.value = '{mr_value}'""")
+    names = cursor.fetchall()
+    if tup in names:
+        print(*names[0])
