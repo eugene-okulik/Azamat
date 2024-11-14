0447cfcb651514f4c8bdcecfae7bd37dc2341827
Azamat <azmmatthew@gmail.com>
2024-11-14 15:51:18 +0300

    fixed search: 5 words before and 5 words after

--- homework/azamat_far/lesson_17_azamat/analyzer.py
+++ homework/azamat_far/lesson_17_azamat/analyzer.py
@@ -8,32 +8,41 @@ parser.add_argument("-n", "--name", help="name for search")
 parser.add_argument("--full", help="type of result", action="store_true")
 args = parser.parse_args()
 
-# Ввод полного пути до лога
-log_path = input("Path to log: ")
 # глобальные константы
-DIRECTORY = os.listdir(log_path)
+DIRECTORY = os.listdir(args.file)
 SEARCH_TEXT = args.name
 
 
 def search(logs, search_name):
-    for FILE in logs:
-        full_path = os.path.join(log_path, FILE)
+    for file in logs:
+        full_path = os.path.join(args.file, file)
         with open(full_path, "r") as log_file:
             for i, line in enumerate(log_file.readlines()):
                 if search_name in line:
-                    print(f'The line: {line} on index: {i} log file: {FILE}')
+                    print(f'Found in the line on index: {i} log file: {file}')
                     # Вывод части строки
-                    first_index = line.index(search_name)
+                    first_index = line.find(search_name)
                     last_index = first_index + len(search_name)
-                    start = first_index - 5
-                    end = last_index + 5
-                    # Блок обработки возникновения ошибок поиска по индексу
-                    if start < 0:
-                        print(line[:end], end='\n')
-                    elif end > len(line):
-                        print(line[start:], end='\n')
-                    else:
-                        print(line[start:end], end='\n')
+                    end_line = line.index(line[-1])
+                    space_count = 0
+                    space_end_count = 0
+                    start = 0
+                    end = -1
+                    # Вычисление 5 слов до искомого
+                    for j in range(first_index, 0, -1):
+                        if line[j - 1] == ' ' or line[j - 1] == ',':
+                            space_count += 1
+                        if space_count == 7:
+                            start = j
+                            break
+                    # Вычисление 5 слов после искомого
+                    for k in range(last_index, end_line):
+                        if line[k + 1] == ' ' or line[k + 1] == ',':
+                            space_end_count += 1
+                        if space_end_count == 6:
+                            end = k + 1
+                            break
+                    print(line[start:end])
                     break
             else:
                 print("Nothing find")
