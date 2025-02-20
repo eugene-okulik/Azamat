ce019c5432bd9126a725f629ed8142dd94e19bc0
Azamat <azmmatthew@gmail.com>
2025-02-20 20:39:27 +0300

    fixed spaces around keyword

--- homework/azamat_far/lesson_22_azamat/locustfile.py
+++ homework/azamat_far/lesson_22_azamat/locustfile.py
@@ -7,11 +7,11 @@ class Objects(HttpUser):
     @task(1)
     def get_all_objects(self):
         self.client.get(
-            '/object', headers = {'Content-Type': 'application/json'}
+            '/object', headers={'Content-Type': 'application/json'}
         )
 
     @task(2)
     def get_one_object(self):
         self.client.get(
-            f'/object/{random.randrange(1, 7)}', headers = {'Content-Type': 'application/json'}
+            f'/object/{random.randrange(1, 7)}', headers={'Content-Type': 'application/json'}
         )
